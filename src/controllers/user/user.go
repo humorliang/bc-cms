9d75045a15981d0efd1915e18562e41d5945d795
humorliang <humorliang@163.com>
2019-01-14 17:50:40 +0800

    login

--- src/controllers/user/user.go
+++ src/controllers/user/user.go
@@ -1,1 +1,18 @@
 package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"controllers"
+)
+
+type LoginForm struct {
+	UserLogin string `json:"user_login",form:"user_login",binding:"required"`
+	UserPass  string `json:"user_pass",form:"user_login",binding:"required"`
+	AuthCode  string `json:"auth_code",form:"auth_code"`
+}
+
+//登录路由
+func Login(c *gin.Context) {
+	ctx := controllers.Context{c}
+	userLogin:=ctx.PostForm("user_login")
+}
--- src/routers/router.go
+++ src/routers/router.go
@@ -1,1 +1,21 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+//路由初始化
+func SetUp(r *gin.Engine) {
+
+	//请求 api v1.0版本
+	rV1 := r.Group("/v1")
+
+	//界面API
+	rV1.GET("/user/login",)
+
+
+	//后台API
+	//rV1Admin := rV1.Group("/admin", middleware.JWTAuth())
+
+
+}
